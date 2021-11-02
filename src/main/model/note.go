e657c365192b23e88e89b040895036457e0f46e5
James Cathcart <47301496+james-cathcart@users.noreply.github.com>
2021-11-02 10:25:06 -0600

    refactored field name

--- src/main/model/note.go
+++ src/main/model/note.go
@@ -4,5 +4,5 @@ type Note struct {
     ID         string `json:"id"`
     Body       string `json:"body"`
     Created    int64  `json:"created"`
-    LastViewed int64  `json:"lastViewed"`
+    Edited int64  `json:"edited"`
 }
